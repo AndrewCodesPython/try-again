8e3f3ee5d399e734ef72edc46c794a53ae9b0ad1
Andrew Escudero <boundlesscreations923@gmail.com>
2023-09-21 12:48:45 -0400

    need-assistance

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar/navbar.js';
 import Header from './components/Header/Header.js';
 import Form from './components/Form/form.js';
-require('dotenv').config() 
+
 
 function App() {
   return (
